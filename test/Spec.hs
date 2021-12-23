d0796883f499aa91b96e8f70c5c517ebef623277
Geometer1729 <16kuhnb@gmail.com>
2021-12-23 11:53:15 -0500

    fix formating

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,21 +1,21 @@
 module Main (main) where
 
-import Test.Tasty (defaultMain, testGroup,localOption)
-import Test.Tasty.Hedgehog(HedgehogTestLimit(HedgehogTestLimit))
+import Test.Tasty (defaultMain, localOption, testGroup)
+import Test.Tasty.Hedgehog (HedgehogTestLimit (HedgehogTestLimit))
 
 import Tactics (shrinkingTactics)
 import UnitTests (makeUnitTests)
 
-import System.Environment(getEnv)
+import System.Environment (getEnv)
 
 main :: IO ()
 main = do
   getEnv "LANG" >>= print
   unitTests <- makeUnitTests
-  defaultMain $ 
-     testGroup
+  defaultMain $
+    testGroup
       "Shrinker"
-      [ localOption (HedgehogTestLimit (Just 100)) shrinkingTactics
-      -- ideally this would be at least 1000 but that seems to cause ram issues
-      , localOption (HedgehogTestLimit (Just   1)) unitTests
+      [ -- ideally this would be at least 1000 but that seems to cause ram issues
+        localOption (HedgehogTestLimit (Just 100)) shrinkingTactics
+      , localOption (HedgehogTestLimit (Just 1)) unitTests
       ]
