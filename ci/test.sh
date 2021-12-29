6c0ccd7e9f8c279e4d4cb4fb01c9a92c375f5e1f
Geometer1729 <16kuhnb@gmail.com>
2021-12-29 16:22:10 -0500

    remove old nix stuff try to fix ci

---
+++ ci/test.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+nix develop -c cabal test
--- nix/pab.nix
+++
@@ -1,7 +0,0 @@
-{ plutus-apps, pkgs ? plutus-apps.pkgs }: rec {
-  plutus_ledger_with_docs =
-    plutus-apps.plutus-apps.haskell.packages.plutus-ledger.components.library.override {
-      doHaddock = true;
-      configureFlags = [ "-f defer-plugin-errors" ];
-    };
-}
