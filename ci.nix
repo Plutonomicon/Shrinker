ad7363a4520b84d231a9c9f50db6c94d991ba832
Geometer1729 <16kuhnb@gmail.com>
2021-12-31 14:22:27 -0500

    improve ci to also build shrinker-tx

--- ci.nix
+++ ci.nix
@@ -1,4 +1,8 @@
 let
 	shrinker = import ./default.nix;
-in
-shrinker.packages.x86_64-linux."shrinker-testing\:test\:shriker-tests"
+in 
+{
+  tests = shrinker.checks.x86_64-linux."shrinker-testing\:test\:shriker-tests";
+  build-tx =shrinker.packages.x86_64-linux."shrinker-tx\:lib\:shrinker-tx";
+}
+
